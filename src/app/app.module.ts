b433caee0a349842cb8f432603a0597bcaae6b19
Marcello <malves548@hotmail.com>
2020-04-09 20:12:02 -0300

    AulaAngular
    Exercicios 1 e 2 aula de Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NOMEDOCOMONENTEComponent } from './nomedocomonente/nomedocomonente.component';
+import { EventosComponent } from './eventos/eventos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NOMEDOCOMONENTEComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule
