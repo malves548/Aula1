19e9e54076b228842a565e511f7e911564428ef5
Marcello <malves548@hotmail.com>
2020-04-12 21:19:41 -0300

    Exercicios 3 a 5
    Exercicios 3 a 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NOMEDOCOMONENTEComponent } from './nomedocomonente/nomedocomonente.component';
 import { EventosComponent } from './eventos/eventos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NOMEDOCOMONENTEComponent,
     EventosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
